ba18e83726fd5b0b33cbfa35fa002637c6582709
bernhard <5026865+kafisatz@users.noreply.github.com>
2020-06-15 13:49:10 +0200

    added testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,37 +5,40 @@ using Test
 using DataFrames
 using CSV
 
-lnk="https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/639388c2cbc2120a14dcf466e85730eb8be498bb/iris.csv"
-fi=download(lnk)
+@testset "Main Tests" begin
 
-df=CSV.read(fi)
+    lnk="https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/639388c2cbc2120a14dcf466e85730eb8be498bb/iris.csv"
+    fi=download(lnk)
 
-v=[:species,:petal_width]
-aggMW=multiwayaggregation(df,v,:sepal_length=>sum)
+    df=CSV.read(fi)
 
-#"manual approach"
-aggReference=combine(DataFrames.groupby(df,[:species,:petal_width]),:sepal_length=>sum)
-agg2=combine(DataFrames.groupby(df,[:species]),:sepal_length=>sum)
-agg3=combine(DataFrames.groupby(df,[:petal_width]),:sepal_length=>sum)
-agg4=DataFrames.combine(df,:sepal_length=>sum)
+    v=[:species,:petal_width]
+    aggMW=multiwayaggregation(df,v,:sepal_length=>sum)
 
-append!(aggReference,agg2,cols=:union)
-append!(aggReference,agg3,cols=:union)
-append!(aggReference,agg4,cols=:union)
+    #"manual approach"
+    aggReference=combine(DataFrames.groupby(df,[:species,:petal_width]),:sepal_length=>sum)
+    agg2=combine(DataFrames.groupby(df,[:species]),:sepal_length=>sum)
+    agg3=combine(DataFrames.groupby(df,[:petal_width]),:sepal_length=>sum)
+    agg4=DataFrames.combine(df,:sepal_length=>sum)
 
-svec=[:sepal_length_sum,:petal_width,:species] 
-sort!(aggReference,svec)
-sort!(aggMW,svec)
-aggMW2=select(aggMW,Not(:_TYPE_))
+    append!(aggReference,agg2,cols=:union)
+    append!(aggReference,agg3,cols=:union)
+    append!(aggReference,agg4,cols=:union)
 
-for i=1:size(aggMW2,1)
-    for j=1:size(aggMW2,2)
-        #@show i,j
-        if ismissing(aggMW2[i,j])
-            @test ismissing(aggReference[i,j])
-        else 
-            @test aggMW2[i,j]==aggReference[i,j]
-        end
+    svec=[:sepal_length_sum,:petal_width,:species] 
+    sort!(aggReference,svec)
+    sort!(aggMW,svec)
+    aggMW2=select(aggMW,Not(:_TYPE_))
+
+    for i=1:size(aggMW2,1)
+        for j=1:size(aggMW2,2)
+            #@show i,j
+            if ismissing(aggMW2[i,j])
+                @test ismissing(aggReference[i,j])
+            else 
+                @test aggMW2[i,j]==aggReference[i,j]
+            end
+        end 
     end 
-end 
 
+end
